0db558191dcd312a754115ecdc46faa252681d1a
Gustavo Amorim <bezerragustavo227@gmail.com>
2025-02-10 16:15:13 -0300

    att

---
+++ model/database.py
@@ -0,0 +1,22 @@
+from json import load
+import mysql.connector 
+from mysql.connector import Error 
+from dotenv import load_dotenv 
+from os import getenv
+
+class Database:
+    def __init__(self):
+        load_dotenv()
+        self.host = getenv('DB_HOST')
+        self.username = getenv('DB_USER')
+        self.password = getenv('DB_PSWD') 
+        self.database = getenv('DB_NAME')   
+        self.connection = None
+        self.cursor = None
+def conectar(self):
+        """Estabelece uma conexão com o banco de dados."""
+        try:
+            self.connection = mysql.connector.connect(
+                host = self.host,
+                database = self.database ) 
+        
